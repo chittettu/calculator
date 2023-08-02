fb9d2ec5aa45f7ea52b1e054e9ad7c49e084a601
chittettu <maheshknair98@gmail.com>
2023-08-02 22:03:21 +0530

    first commit

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
 from flask import Flask, render_template,request,jsonify
 import json
 app=Flask(__name__)
-@app.route('/')
 
+
+@app.route('/')
 def welcome():
     return "welcome to flask"
 
-@app.route('/cal', methods=["GET"])
-def math_calculator():
-    operation=request.json["operation"]
-    number1=request.json["number1"]
-    number2=request.json["number2"]
+@app.route('/calculate',methods=['POST','GET'])
+def calculate():
+    if request.method=='GET':
+        return render_template('calculate.html')
+    else:
+        number1=float(request.form["number1"])
+        number2=float(request.form["number2"])
+        operation=request.form["operation"]
 
-    if operation=="add":
-        result=int(number1)+int(number2)
-    elif operation=="multiplication":
-        result=int(number1)*int(number2)
-    elif operation=="division":
-        result=int(number1)/int(number2)
-    elif operation=="subtraction":
-        result=int(number1)-int(number2)
-    return f"the operation is {operation} and result is {result}"
+
+        if operation=="add":
+            result=int(number1)+int(number2)
+        elif operation=="multiplication":
+             result=int(number1)*int(number2)
+        elif operation=="division":
+            result=int(number1)/int(number2) 
+        elif operation=="subtraction":
+            result=int(number1)-int(number2)
+
+    return render_template('result.html',results=result)
+
+
+   
+
 
     
 
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
 
